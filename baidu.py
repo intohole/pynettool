76c74d22b2a7425ba19069f79fd1ce5af478f520
lixuze <lixuze@iyuewe.com>
2013-08-09 16:33:27 +0800

    添加ip归属地查询接口

--- baidu.py
+++ baidu.py
@@ -1,22 +1,49 @@
-#coding=utf-8
+# coding=utf-8
 
 
 
-
+import threading
 import util
  
 suggesturl = 'http://esug.baidu.com/su?'
+ip_url = "http://opendata.baidu.com/api.php?"
  
- 
-def suggest(word):
+def get_suggest_word(word):
     query = {"wd":word,
              "p":3,
              "t":util.timems()}
     url = util.queryurl(suggesturl, query)
-    return util.get_url_data(url,"")
+    return util.get_url_data(url, "")
 
+def get_bd_ip_local(ip):
+    query = {"query":ip,
+            "co":"",
+            "resource_id":"6006",
+            "t":"1376035704061",
+            "ie":"utf8",
+            "oe":"gbk",
+            "cb":"bd__cbs__yk2g9h",
+            "format":"json",
+            "tn":"baidu"}
+    return util.get_url_info(query, ip_url , code = "gbk")
+
+
+
+def get_ip_location(ip):
+    return util.jsonstrtodict(util.getjson(get_bd_ip_local(ip)))['data'][0]['location']
+
+
+class test(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        while True:
+            print util.getjson(get_bd_ip_local("220.181.111.86"))
+            
 if __name__ == "__main__":
-    pass
+    print get_ip_location("220.181.111.86")
 
 
 
